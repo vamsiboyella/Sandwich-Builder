1623c93d19139bc5ac0aa0919aeb19ecf6b8dea6
vamsiboyella <60796050+vamsiboyella@users.noreply.github.com>
2020-08-03 19:35:35 -0400

    Update auth.js

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -48,9 +48,9 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         };
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAnbrX31FU_q2XoSPchLs50_ELBX14uHsM';
+        let url = 'Use your concerned google api  sign up authneticator';
         if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAnbrX31FU_q2XoSPchLs50_ELBX14uHsM';
+            url = 'Use your concerned google api  sign in authneticator';
         }
         axios.post(url, authData)
             .then(response => {
